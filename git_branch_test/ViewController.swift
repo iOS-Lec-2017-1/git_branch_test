6add065d2f497959a8c754a6bea6b18a836a0044
김종현 <jhkim@dit.ac.kr>
2017-09-10 20:33:36 +0900

    2nd merge

--- git_branch_test/ViewController.swift
+++ git_branch_test/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("git branch test!")
+        print("2nd modify")
     }
 
     override func didReceiveMemoryWarning() {
