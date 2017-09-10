53a43cdda3993917b37a2cda8864fb4242aae7bf
김종현 <jhkim@dit.ac.kr>
2017-09-10 20:31:09 +0900

    2nd modify

--- git_branch_test/ViewController.swift
+++ git_branch_test/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("git branch test!")
+        print("2nd modify")
     }
 
     override func didReceiveMemoryWarning() {
